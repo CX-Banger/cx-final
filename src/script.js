463f364355c7368d5068f5293bc03410f2514e3e
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-10-28 17:37:36 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -135,11 +135,11 @@ function renderFeatured(){
   featuredDiv.innerHTML='';
 
   const featuredAlbums = [
-    { title: "Olala", artist: "NAN", year: "NAN", cover: `${GITHUB_BASE_URL}/media/artiste1/cover1.jpg`, artistIndex: 0 },
-    { title: "YHWH", artist: "Synaï", year: "Synaï", cover: `${GITHUB_BASE_URL}/media/artiste2/cover1.jpg`, artistIndex: 1 },
+    { title: "Sans Effet", artist: "NAN", year: "NAN", cover: `${GITHUB_BASE_URL}/media/artiste1/cover21.jpg`, artistIndex: 0 },
+    { title: "The King ft Elihem", artist: "Synaï ft Elihem", year: "Synaï ft Elihem", cover: `${GITHUB_BASE_URL}/media/artiste2/cover12.jpg`, artistIndex: 1 },
     { title: "In God", artist: "Elihem", year: "Elihem", cover: `${GITHUB_BASE_URL}/media/artiste3/cover1.jpg`, artistIndex: 2 },
     { title: "Louange à Mon Dieu", artist: "Sara", year: "Sara", cover: `${GITHUB_BASE_URL}/media/artiste4/cover1.jpg`, artistIndex: 3 },
-    { title: "Cendrillon", artist: "Eilynn", year: "Eilynn", cover: `${GITHUB_BASE_URL}/media/artiste5/cover1.jpg`, artistIndex: 4 },
+    { title: "Flame of Life", artist: "Eilynn", year: "Eilynn", cover: `${GITHUB_BASE_URL}/media/artiste5/cover7.jpg`, artistIndex: 4 },
     { title: "Ma Carrière", artist: "Melohim", year: "Melohim", cover: `${GITHUB_BASE_URL}/media/artiste6/cover1.jpg`, artistIndex: 5 },
     { title: "Tiim 1", artist: "Tiim", year: "Tiim", cover: `${GITHUB_BASE_URL}/media/artiste7/cover1.jpg`, artistIndex: 6 },
     { title: "Math 1", artist: "Math", year: "Math", cover: `${GITHUB_BASE_URL}/media/artiste8/cover1.jpg`, artistIndex: 7 }
