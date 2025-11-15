551d3378a11be785e321aaa85f5ae8204933630b
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-11-15 11:12:37 +0100

    Create .js

---
+++ .js
@@ -0,0 +1,67 @@
+~/project
+❯ npm install
+
+added 273 packages in 2m
+
+69 packages are looking for funding
+  run `npm fund` for details
+
+~/project 2m 26s
+❯ npm run dev
+
+> vite-react-typescript-starter@0.0.0 dev
+> vite
+
+
+  VITE v5.4.21  ready in 3287 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h + enter to show help
+10:40:07 [vite] page reload index.html
+10:47:41 [vite] hmr update /src/style.css?direct
+10:48:16 [vite] hmr update /src/style.css?direct (x2)
+10:48:36 [vite] hmr update /src/style.css?direct (x3)
+10:50:54 [vite] page reload src/script.js
+10:56:10 [vite] page reload src/script.js (x2)
+10:57:22 [vite] page reload src/script.js (x3)
+11:03:00 [vite] page reload src/script.js (x4)
+11:07:12 [vite] page reload src/script.js (x5)
+11:08:36 [vite] .env changed, restarting server...
+Re-optimizing dependencies because lockfile has changed
+11:08:36 [vite] server restarted.
+11:10:07 [vite] .env changed, restarting server...
+11:10:08 [vite] server restarted.
+11:10:46 [vite] Internal server error: Failed to parse source for import analysis because the content contains invalid JS syntax. If you are using JSX, make sure to name the file with the .jsx or .tsx extension.
+  Plugin: vite:import-analysis
+  File: /home/project/src/script.js:1730:2
+  1729|  }
+  1730|  
+  1731|  }
+     |  ^
+  1732|  
+      at TransformPluginContext._formatError (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49308:41)
+      at TransformPluginContext.error (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49303:16)
+      at TransformPluginContext.transform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:64309:14)
+      at async PluginContainer.transform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49149:18)
+      at async loadAndTransform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:52028:27)
+11:10:54 [vite] Internal server error: Failed to parse source for import analysis because the content contains invalid JS syntax. If you are using JSX, make sure to name the file with the .jsx or .tsx extension.
+  Plugin: vite:import-analysis
+  File: /home/project/src/script.js:1730:2
+  1729|  }
+  1730|  
+  1731|  }
+     |  ^
+  1732|  
+      at TransformPluginContext._formatError (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49308:41)
+      at TransformPluginContext.error (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49303:16)
+      at TransformPluginContext.transform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:64309:14)
+      at async PluginContainer.transform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:49149:18)
+      at async loadAndTransform (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:52028:27)
+      at async viteTransformMiddleware (file:///home/project/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:62156:24)
+^C
+
+~/project 31m 24s
+❯ 
+
+~/project
